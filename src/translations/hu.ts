9d3fae8771b79ec2c28e9e72ef2b7726765c0b97
Gergely-H <gergely.huszar@proton.me>
2023-05-24 17:06:24 +0200

    add translations/hu.ts

---
+++ src/translations/hu.ts
@@ -0,0 +1,17 @@
+const t = {
+  navigation: {
+    items: {
+      news: "Hírek",
+      dogs: "Kutyák",
+      cats: "Macskák",
+      successStories: "Sikertörténetek",
+      supporters: "Támogatók",
+      donation: "Adományozás",
+      contact: "Kapcsolat",
+      volunteer: "Önkéntesség",
+      aboutUs: "Rólunk",
+    },
+  },
+};
+
+export default t;
