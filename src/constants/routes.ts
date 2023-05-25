bfa990e6e649e60003306b144764cfbf3188dfcd
Gergely-H <gergely.huszar@proton.me>
2023-05-25 18:50:39 +0200

    add routes constants

---
+++ src/constants/routes.ts
@@ -0,0 +1,13 @@
+const routes = {
+  news: "/hirek",
+  dogs: "/kutyak",
+  cats: "/cicak",
+  successStories: "/sikertortenetek",
+  supporters: "/tamogatok",
+  donation: "/adomanyozas",
+  contact: "/kapcsolat",
+  volunteer: "/onkentesseg",
+  aboutUs: "/rolunk",
+};
+
+export default routes;
