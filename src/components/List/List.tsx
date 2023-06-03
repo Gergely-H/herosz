0c8881ea546973dda0283815bd396a7e6ddd0791
Gergely-H <gergely.huszar@proton.me>
2023-06-03 16:58:04 +0200

    add List component's skeleton

---
+++ src/components/List/List.tsx
@@ -0,0 +1,9 @@
+import type { FC } from "react";
+
+type Props = {};
+
+const List: FC<Props> = () => {
+  return <></>;
+};
+
+export default List;
---
+++ src/components/List/ListItem.tsx
@@ -0,0 +1,9 @@
+import type { FC } from "react";
+
+type Props = {};
+
+const ListItem: FC<Props> = () => {
+  return <></>;
+};
+
+export default ListItem;
