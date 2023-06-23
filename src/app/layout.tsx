301f797ad0605ecb18eaeaee2497d81fa89c80b2
Gergely-H <gergely.huszar@proton.me>
2023-06-23 14:14:51 +0200

    update RootLayout metadata's description

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,25 +1,26 @@
-import Footer from "@/components/Footer";
-import Header from "@/components/Header";
-import type { MetadataConfig } from "@/types/nextTypes";
-import { Inter } from "next/font/google";
-import type { FC, PropsWithChildren } from "react";
-import "./globals.css";
+import Footer from '@/components/Footer';
+import Header from '@/components/Header';
+import type { MetadataConfig } from '@/types/nextTypes';
+import { Inter } from 'next/font/google';
+import type { FC, PropsWithChildren } from 'react';
+import './globals.css';
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] });
 
-const META_TITLE = "Herosz Budapesti Állatotthon";
+const META_TITLE = 'Herosz Budapesti Állatotthon';
 const metadataConfig: MetadataConfig = {
   metadata: {
     title: {
       default: META_TITLE,
       template: `%s - ${META_TITLE}`,
     },
-    description: "Herosz Budapesti Állatotthon weboldala.",
+    description:
+      'Herosz Budapesti Állatotthon - Herman Ottó Magyar Országos Állat- és Természetvédő Egyesület budapesti állatotthona',
     /**
      * This metadata is needed to avoid 403 response when fetching images of Google Docs.
      * https://stackoverflow.com/a/74495028
      */
-    referrer: "no-referrer",
+    referrer: 'no-referrer',
   },
 };
 
